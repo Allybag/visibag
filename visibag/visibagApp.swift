1e1c9061bbbf100a53d10b411139c7d18c319432
Ally <bagkingally@gmail.com>
2024-10-04 20:43:09 -0400

    Initial Commit

---
+++ visibag/visibagApp.swift
@@ -0,0 +1,17 @@
+//
+//  visibagApp.swift
+//  visibag
+//
+//  Created by Alex A on 4/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct visibagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ visibagTests/visibagTests.swift
@@ -0,0 +1,16 @@
+//
+//  visibagTests.swift
+//  visibagTests
+//
+//  Created by Alex A on 4/10/2024.
+//
+
+import Testing
+
+struct visibagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
