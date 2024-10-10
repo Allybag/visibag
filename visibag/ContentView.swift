c294a2345a74b79c15d9eea8ab4556cd66ebe9c6
Ally <bagkingally@gmail.com>
2024-10-10 18:47:03 -0400

    Extract data points from JSON and plot all series on chart
    The JSON is currently hardcoded in Data.swift

--- visibag/ContentView.swift
+++ visibag/ContentView.swift
@@ -8,23 +8,72 @@
 import SwiftUI
 import Charts
 
-struct Point : Identifiable {
+struct Point : Codable, Identifiable {
     let x: Float
     let y: Float
-    
+
     var id: Int { x.hashValue }
+
+    init(from decoder: Decoder) throws {
+        var values = try decoder.unkeyedContainer()
+        x = try values.decode(Float.self)
+        y = try values.decode(Float.self)
+    }
+}
+
+
+struct Message: Codable, Identifiable {
+    let key: String
+    let data: Dictionary<String, [Point]>
+    let events: [String]
+    
+    var id: Int { key.hashValue }
+}
+
+struct NamedSeries : Identifiable
+{
+    let name: String
+    let series: [Point]
+
+    var id : Int { name.hashValue }
 }
 
 struct ContentView: View {
-    let points = [Point](arrayLiteral: Point(x: 0, y: 1), Point(x: 1, y: 2), Point(x: 2, y: 3))
-    
-    var body: some View {
-        Chart (points) { point in
-            LineMark(
-                x: .value("Time", point.x),
-                y: .value("Value", point.y)
-            )
+    var data: [NamedSeries] {
+        do {
+            let messages = try JSONDecoder().decode([Message].self, from: Data(json.utf8))
+            print("Mesaages: \(messages)")
+            if let message = messages.first {
+                var result = [NamedSeries]()
+                for (key, value) in message.data {
+                    result.append(NamedSeries(name: key, series: value))
+                }
+                return result
+            }
+            return []
         }
+        catch
+        {
+            print("Unexpected error: \(error).")
+        }
+
+        return []
+    }
+
+    var body: some View {
+        Chart (data) { series in
+            ForEach (series.series) { point in
+                LineMark(
+                    x: .value("Time", point.x),
+                    y: .value("Value", point.y)
+                )
+            }
+            .foregroundStyle(by: .value("Name", series.name))
+        }
+        .chartYScale(domain: .automatic(includesZero: false))
+        .chartXScale(domain: .automatic(includesZero: false))
+        .chartYAxisLabel("Price")
+        .chartXAxisLabel("Time")
         .padding()
     }
 }
